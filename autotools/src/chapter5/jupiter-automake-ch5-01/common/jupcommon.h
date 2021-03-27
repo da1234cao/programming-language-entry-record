498e7197600bd3052ea3347bd3d027ccd2826f78
da1234cao <17355051286@163.com>
2021-03-27 21:50:18 +0800

    添加autotools第五章automake内容

---
+++ autotools/src/chapter5/jupiter-automake-ch5-01/common/jupcommon.h
@@ -0,0 +1,8 @@
+#ifndef JUPCOMMON_H
+#define JUPCOMMON_H
+
+int print_routine(const char * name);
+
+
+#endif
+
--- autotools/src/chapter5/jupiter-automake-ch5-01/src/main.c
+++ autotools/src/chapter5/jupiter-automake-ch5-01/src/main.c
@@ -1,15 +1,5 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <pthread.h>
-
-static void* print_it(void* data){
-    printf("Hello from %s!\n",(const char *)data);
-    return 0;
-}
+#include "jupcommon.h"
 
 int main(int argc,char* argv[]){
-    pthread_t tid;
-    pthread_create(&tid,0,print_it,argv[0]);
-    pthread_join(tid,0);
-    return 0;
+    return print_routine(argv[0]);
 }
